b5aa2c11275a913490def573339cb0ff5b965102
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-03-02 22:21:13 +0300

    Move source to src

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  srcDir: './src',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
