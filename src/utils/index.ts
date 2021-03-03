677838471b76fb23f43c2c90749f44b43ab4a09c
Ildar Timerbaev <dkildar@gmail.com>
2021-03-03 21:20:32 +0300

    Added process-value.util.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
 export * from './get-error-message.util'
 export * from './get-vests-to-sp.util'
+export * from './legacy-to-hive-uri.util'
+export * from './process-value.util'
---
+++ src/utils/process-value.util.ts
@@ -0,0 +1,23 @@
+export function processValue(schema: any, key: string, value: any, { vestsToSP }: any): any {
+  const { type, defaultValue, maxLength } = schema[key]
+  const realValue = !value && typeof defaultValue !== 'undefined' ? defaultValue : value
+  switch (type) {
+    case 'amount':
+      if (realValue.indexOf('VESTS') !== -1) return `${parseFloat(realValue).toFixed(6)} VESTS`
+      if (realValue.indexOf('HP') !== -1)
+        return `${(parseFloat(realValue) / vestsToSP).toFixed(6)} VESTS`
+      if (realValue.indexOf('HIVE') !== -1) return `${parseFloat(realValue).toFixed(3)} HIVE`
+      if (realValue.indexOf('HBD') !== -1) return `${parseFloat(realValue).toFixed(3)} HBD`
+      return realValue
+    case 'int':
+      return parseInt(realValue, 10)
+    case 'bool':
+      if (value === 'false' || value === false) return false
+      return realValue
+    case 'string':
+      if (maxLength) return realValue.substring(0, Math.min(realValue.length, maxLength - 1))
+      return realValue
+    default:
+      return realValue
+  }
+}
