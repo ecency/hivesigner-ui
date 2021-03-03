d3f76412c812dd418b5e4f29bcaa681e8b40041f
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-03-03 19:27:34 +0300

    Added testing store module

---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div @click="changeTitle">{{ title }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { AuthModule } from '~/store'
+
+@Component
+export default class Index extends Vue {
+  get title() {
+    return AuthModule.title
+  }
+
+  changeTitle() {
+    AuthModule.setTitle()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,9 +1,50 @@
-import { Module, VuexModule } from 'nuxt-property-decorator'
+import { Module, VuexAction, VuexModule, VuexMutation } from 'nuxt-property-decorator'
 
 @Module({
   stateFactory: true,
   namespaced: true,
-  name: 'auth',
+  name: 'auth'
 })
 export default class Auth extends VuexModule {
+  public keys: any = {}
+  public account: any = {}
+
+  public get username(): string {
+    return this.account.username
+  }
+
+  @VuexMutation
+  public setUser({result, keys}: any): void {
+    this.keys = keys
+    this.account = result
+  }
+
+  @VuexMutation
+  public clearUser(): void {
+    this.keys = {}
+    this.account = {}
+  }
+
+  @VuexMutation
+  public setAccount(account: any): void {
+    this.account = account
+  }
+
+  @VuexAction
+  public async login({username, keys}: any): Promise<any> {
+    const key = keys.owner || keys.active || keys.posting || keys.memo
+    const valid = await credentialsValid(username, key)
+
+    if (!valid) {
+      throw new Error('Invalid credentials')
+    }
+
+    const result = await client.database.getAccounts([username])
+    commit('login', {result: result[0], keys})
+
+    idleDetector.start(rootState.settings.timeout * 60 * 1000, () => {
+      idleDetector.stop()
+      dispatch('logout')
+    })
+  }
 }
