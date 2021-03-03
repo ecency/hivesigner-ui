c6228a70bc2ba5bc687a35420e844bcd39934af0
Ildar Timerbaev <dkildar@gmail.com>
2021-03-03 21:29:02 +0300

    Added get-lowest-authority-required.util.ts

---
+++ src/utils/get-lowest-authority-required.util.ts
@@ -0,0 +1,17 @@
+import { Transaction } from '@hiveio/dhive'
+import { OPERATIONS } from '~/consts'
+
+// TODO: Return enum of authority
+export function getLowestAuthorityRequired(tx: Transaction): string | null {
+  let authority: string | null = null
+  tx.operations.forEach(operation => {
+    if (OPERATIONS[operation[0]] && OPERATIONS[operation[0]].authority) {
+      if (OPERATIONS[operation[0]].authority === 'owner') authority = 'owner'
+      if (OPERATIONS[operation[0]].authority === 'active') authority = 'active'
+      if (OPERATIONS[operation[0]].authority === 'posting' && authority !== 'active') {
+        authority = 'posting'
+      }
+    }
+  })
+  return authority
+}
