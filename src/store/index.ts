f996bcb95a7a14eb0791491a506e3d96e463be6f
Ildar Timerbaev <dkildar@gmail.com>
2021-03-06 15:55:26 +0300

    Added persistent forms module

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,15 +3,18 @@ import Auth from '~/store/auth'
 import { getModule } from 'nuxt-property-decorator'
 import Settings from '~/store/settings'
 import Ui from '~/store/ui'
+import PersistentForms from '~/store/persistent-forms'
 
 let AuthModule: Auth
 let SettingsModule: Settings
 let UiModule: Ui
+let PersistentFormsModule: PersistentForms
 
 const initializer = (store: Store<any>) => {
   AuthModule = getModule<Auth>(Auth, store)
   SettingsModule = getModule<Settings>(Settings, store)
   UiModule = getModule<Ui>(Ui, store)
+  PersistentFormsModule = getModule<PersistentForms>(PersistentForms, store)
 }
 export const plugins = [initializer]
 
---
+++ src/store/persistent-forms.ts
@@ -0,0 +1,59 @@
+import { Module, VuexModule, VuexMutation, Vue } from 'nuxt-property-decorator'
+
+// Rationale behind those persistent forms is exactly the same as Metamask's:
+// https://github.com/MetaMask/metamask-extension/blob/develop/docs/form_persisting_architecture.md
+// The only difference is in the implementation (Metamask uses localStorage), in this case we
+// save form state to Vuex which is synced to the background script.
+@Module({
+  stateFactory: true,
+  namespaced: true,
+  name: 'persistentForms'
+})
+export default class PersistentForms extends VuexModule {
+  public login: any = {
+    username: '',
+    key: ''
+  }
+  public import: any = {
+    step: 1,
+    username: '',
+    password: '',
+    key: '',
+    keyConfirmation: ''
+  }
+
+  @VuexMutation
+  public saveLoginUsername(username: string): void {
+    Vue.set(this.login, 'username', username)
+  }
+
+  @VuexMutation
+  public saveLoginKey(key: string): void {
+    Vue.set(this.login, 'key', key)
+  }
+
+  @VuexMutation
+  public saveImportStep(step: number): void {
+    Vue.set(this.login, 'step', step)
+  }
+
+  @VuexMutation
+  public saveImportUsername(username: string): void {
+    Vue.set(this.import, 'username', username)
+  }
+
+  @VuexMutation
+  public saveImportPassword(password: string): void {
+    Vue.set(this.import, 'password', password)
+  }
+
+  @VuexMutation
+  public saveImportKey(key: string): void {
+    Vue.set(this.import, 'key', key)
+  }
+
+  @VuexMutation
+  public saveImportKeyConfirmation(keyConfirmation: string): void {
+    Vue.set(this.import, 'keyConfirmation', keyConfirmation)
+  }
+}
