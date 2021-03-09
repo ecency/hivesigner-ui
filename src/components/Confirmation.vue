a21af9f42b107f018b212c4462ba0958e1fec619
Ildar Timerbaev <dkildar@gmail.com>
2021-03-09 23:01:07 +0300

    Added Center.vue and Confirmation.vue

---
+++ src/components/Confirmation.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flash flash-success mb-4">
+    Your transaction is on the way! Here is the ID of the transaction:
+    <a :href="`https://hiveblocks.com/tx/${id}`" target="_blank">{{ id }}</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class Confirmation extends Vue {
+  @Prop({
+    type: String,
+  })
+  private id!: string
+}
+</script>
