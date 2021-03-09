c8d12a2ec60d2f4291812069c28687fca0961d21
Ildar Timerbaev <dkildar@gmail.com>
2021-03-09 22:25:07 +0300

    Added Search.vue

---
+++ src/components/Search.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search px-4">
+    <span class="iconfont icon-search"/>
+    <input
+      :value="value"
+      :placeholder="placeholder"
+      @input="handleInput"
+      type="text"
+      autocorrect="off"
+      autocapitalize="none"
+      class="px-4 py-3 border-0 input-lg"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class Search extends Vue {
+  @Prop()
+  private value!: string
+
+  @Prop()
+  private placeholder!: string
+
+  private handleInput(e: Event): void {
+    this.$emit('input', (e.target as HTMLInputElement).value)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../assets/scss/vars';
+
+  .search {
+    display: flex;
+    align-items: center;
+
+    & > .icon-search {
+      color: $input-border-color;
+      font-size: 20px;
+    }
+
+    & > input {
+      flex: 1;
+    }
+  }
+</style>
