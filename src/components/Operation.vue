8a94db79515ed7b836b371762f582bfa3e0caeff
Ildar Timerbaev <dkildar@gmail.com>
2021-03-09 22:44:59 +0300

    Added Operation.vue

---
+++ src/components/Operation.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Box operation mb-4">
+    <OperationHeader :operation="operation[0]" />
+    <div class="Box-row">
+      <div v-for="(value, key) in operation[1]" :key="key">
+        <p>
+          <b class="form-label v-align-top">{{ schema[key].name || key }}</b>
+          <OperationValue :value="value" :path="operation[0] + '.schema.' + key" />
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import { OPERATIONS } from '~/consts'
+import OperationHeader from '~/components/Operation/OperationHeader.vue'
+import OperationValue from '~/components/Operation/OperationValue.vue'
+
+@Component({
+  components: { OperationValue, OperationHeader }
+})
+export default class Operation extends Vue {
+  @Prop()
+  private operation!: any
+
+  private get schema(): any {
+    return OPERATIONS[`${this.operation[0]}.schema`] || {}
+  }
+}
+</script>
