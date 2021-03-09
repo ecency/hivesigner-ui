0cf865ea5c8b943debddf5cf2ddc3c8ab4bd96e3
Ildar Timerbaev <dkildar@gmail.com>
2021-03-09 22:21:28 +0300

    Added Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="footer mb-4">
+    <ul>
+      <li class="d-inline mx-2">
+        <router-link to="/apps">Apps</router-link>
+      </li>
+      <li class="d-inline mx-2">
+        <router-link to="/accounts">Accounts</router-link>
+      </li>
+      <li class="d-inline mx-2">
+        <router-link to="/settings">Settings</router-link>
+      </li>
+      <li class="d-inline mx-2">
+        <router-link to="/developers">Developers</router-link>
+      </li>
+      <li class="d-inline mx-2">
+        <router-link to="/about">About</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<style scoped lang="scss">
+  @import '../assets/scss/vars';
+
+  .footer {
+    ul {
+      list-style: none;
+
+      a {
+        color: $input-border-color;
+      }
+    }
+  }
+</style>
