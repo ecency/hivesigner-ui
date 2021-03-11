4cc84693241fe2fa03389748cf30de9c90225000
Ildar Timerbaev <dkildar@gmail.com>
2021-03-11 21:13:16 +0300

    Added vue i18n

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/vue-i18n.ts',
     '~/plugins/idle-detector.ts',
     '~/plugins/hivesigner.ts',
     '~/plugins/vue-ui.ts'
