7cfcc749e5ec9b0c5a43f7c43485b52ea7b36fc3
Ildar Timerbaev <dkildar@gmail.com>
2021-03-11 20:31:56 +0300

    Added Avatar.vue

---
+++ src/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="text-center">
+    <a @click="$emit('select')">
+      <span class="mb-2 d-block">
+        <Avatar :username="username" :size="60"/>
+      </span>
+      {{ username }}
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+import Avatar from '~/components/Avatar.vue'
+
+@Component({
+  Avatar,
+})
+export default class App extends Vue {
+  @Prop({
+    type: String,
+    default: '',
+  })
+  private username!: string
+}
+</script>
