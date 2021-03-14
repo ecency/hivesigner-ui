5290f390dd412c17b5ad54d36cd3e5f0034cbe38
Ildar Timerbaev <dkildar@gmail.com>
2021-03-14 20:25:16 +0300

    Added Home.vue

---
+++ src/pages/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hero height-full">
+    <Center>
+      <span class="logo iconfont icon-hivesigner"/>
+      <h4 class="m-0">hivesigner</h4>
+      <div class="mt-4 mb-7">
+        <h2 class="mb-6">You're in control of your keys.</h2>
+        <div class="mb-6">
+          <router-link
+            to="/login?redirect=accounts"
+            class="rounded-2 px-3 py-3 bg-primary text-white button-link"
+          >
+            <span class="iconfont icon-shield"/>
+            Get started
+          </router-link>
+        </div>
+        <!-- <p>
+          Hivesigner neither stores nor has access to your private keys.
+        </p> -->
+      </div>
+      <Footer/>
+    </Center>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { PACKAGE } from '~/consts'
+
+@Component
+export default class Home extends Vue {
+  private pkg = PACKAGE
+}
+</script>
