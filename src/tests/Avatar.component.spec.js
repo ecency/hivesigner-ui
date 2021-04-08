7ee6e0d6efcb823626578c308adf71e7968e6769
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-04-08 22:31:59 +0300

    Added unit tests for Avatar component

---
+++ src/tests/Avatar.component.spec.js
@@ -0,0 +1,62 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import Avatar from '@/components/Avatar'
+
+describe('AvatarComponent', function () {
+  let localVue
+  let router
+  let wrapper
+  let store
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(VueRouter)
+    localVue.use(Vuex)
+    router = new VueRouter()
+    store = new Vuex.Store({
+      modules: {
+        auth: {
+          keys: {}
+        }
+      },
+      getters: {
+        username: jest.fn()
+      }
+    })
+
+    wrapper = shallowMount(Avatar, {
+      localVue,
+      router,
+      store,
+      propsData: {
+        username: 'test1',
+      },
+    })
+  })
+
+  it('should create', function () {
+    expect(wrapper).toBeTruthy()
+  })
+
+  it('should set background image url with username', async function () {
+    await wrapper.setProps({
+      username: 'test'
+    })
+    expect(wrapper.find('.avatar').element.style.backgroundImage)
+      .toBe('url(https://images.ecency.com/u/test/avatar/small)')
+  })
+
+  it('should set avatar dimensions by size', async function () {
+    await wrapper.setProps({
+      size: 10
+    })
+    expect(wrapper.find('.avatar').element.style.width).toBe('10px')
+    expect(wrapper.find('.avatar').element.style.height).toBe('10px')
+  })
+
+  it('should set avatar dimensions by default size', async function () {
+    expect(wrapper.find('.avatar').element.style.width).toBe('32px')
+    expect(wrapper.find('.avatar').element.style.height).toBe('32px')
+  })
+})
