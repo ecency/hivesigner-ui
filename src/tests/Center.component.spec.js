c2e982adca39657cfe71ddcdec5b4a536fe9ec09
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-04-08 22:27:41 +0300

    Added unit tests for Center component

---
+++ src/tests/Center.component.spec.js
@@ -0,0 +1,38 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import Center from '@/components/Center'
+
+describe('CenterComponent', function () {
+  let localVue
+  let router
+  let wrapper
+  let store
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(VueRouter)
+    localVue.use(Vuex)
+    router = new VueRouter()
+    store = new Vuex.Store({
+      modules: {
+        auth: {
+          keys: {}
+        }
+      },
+      getters: {
+        username: jest.fn()
+      }
+    })
+
+    wrapper = shallowMount(Center, {
+      localVue,
+      router,
+      store,
+    })
+  })
+
+  it('should create', function () {
+    expect(wrapper).toBeTruthy()
+  })
+})
