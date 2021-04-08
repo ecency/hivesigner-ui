9727e0f32b4a852e5610cc032824228498018004
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-04-08 21:10:23 +0300

    Get rid of unused dashboard component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,29 +1,12 @@
 <template>
-  <component :is="component"/>
+  <home />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import { isWeb } from '~/utils'
-import Home from '~/components/Index/Home'
-import Dashboard from '~/components/Index/Dashobard'
-import checkForAuthorized from '~/middleware/auth'
-import { Context } from '@nuxt/types'
 
 @Component
 export default class Index extends Vue {
-  private get component(): any {
-    if (isWeb()) {
-      return Home
-    }
-    checkForAuthorized({
-      redirect: this.$router.replace,
-      store: this.$store,
-      params: this.$route.params,
-      route: this.$route,
-    } as Context)
-    return Dashboard
-  }
 }
 </script>
 
