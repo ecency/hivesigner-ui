f65be183c5fec933208649407ab4ae9b65af437b
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-04-08 22:23:36 +0300

    Added unit tests for Footer

---
+++ src/tests/Footer.component.spec.js
@@ -0,0 +1,38 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import Footer from '@/components/Footer'
+
+describe('FooterComponent', function () {
+  let localVue
+  let router
+  let wrapper
+  let store
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(VueRouter)
+    localVue.use(Vuex)
+    router = new VueRouter()
+    store = new Vuex.Store({
+      modules: {
+        auth: {
+          keys: {}
+        }
+      },
+      getters: {
+        username: jest.fn()
+      }
+    })
+
+    wrapper = shallowMount(Footer, {
+      localVue,
+      router,
+      store,
+    })
+  })
+
+  it('should create', function () {
+    expect(wrapper).toBeTruthy()
+  })
+})
