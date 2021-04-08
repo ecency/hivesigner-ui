682623f8a38b2529a8dc1668a52726dbea160cf6
Ildar Timerbaev <ildar.timerbaev@piano.io>
2021-04-08 21:10:10 +0300

    Added unit tests for Home

--- src/components/Index/Home.vue
+++ src/components/Index/Home.vue
@@ -1,36 +1,33 @@
 <template>
   <div class="hero height-full">
     <Center>
-      <span class="logo iconfont icon-hivesigner"/>
-      <h4 class="m-0">hivesigner</h4>
+      <span class="logo iconfont icon-hivesigner" />
+      <h4 class="m-0">
+        hivesigner
+      </h4>
       <div class="mt-4 mb-7">
-        <h2 class="mb-6">You're in control of your keys.</h2>
+        <h2 class="mb-6">
+          You're in control of your keys.
+        </h2>
         <div class="mb-6">
           <router-link
             to="/login?redirect=accounts"
             class="rounded-2 px-3 py-3 bg-primary text-white button-link"
           >
-            <span class="iconfont icon-shield"/>
+            <span class="iconfont icon-shield" />
             Get started
           </router-link>
         </div>
-        <!-- <p>
-          Hivesigner neither stores nor has access to your private keys.
-        </p> -->
       </div>
-      <Footer/>
+      <Footer />
     </Center>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import { PACKAGE } from '../../consts'
 
-@Component({
-  middleware: ['isWeb']
-})
+@Component
 export default class Home extends Vue {
-  private pkg = PACKAGE
 }
 </script>
---
+++ src/tests/Home.component.spec.js
@@ -0,0 +1,38 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import Home from '@/components/Index/Home'
+
+describe('DeveloperItemComponent', function () {
+  let localVue
+  let router
+  let wrapper
+  let store
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(VueRouter)
+    localVue.use(Vuex)
+    router = new VueRouter()
+    store = new Vuex.Store({
+      modules: {
+        auth: {
+          keys: {}
+        }
+      },
+      getters: {
+        username: jest.fn()
+      }
+    })
+
+    wrapper = shallowMount(Home, {
+      localVue,
+      router,
+      store,
+    })
+  })
+
+  it('should create', function () {
+    expect(wrapper).toBeTruthy()
+  })
+})
