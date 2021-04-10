8e915df99b36dd769f9b2de2a453efadc2bf2633
Ildar Timerbaev <dkildar@gmail.com>
2021-04-10 23:02:34 +0300

    Return back error component

---
+++ src/components/Error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flash flash-error mb-4">
+    Oops, something went wrong.
+    <span v-if="message">
+      Here is the error message:
+      <br/><b>"{{ message }}"</b>
+    </span>
+    <span v-else>Please try again later.</span>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import { getErrorMessage } from '~/utils'
+
+@Component
+export default class Error extends Vue {
+  @Prop()
+  private error!: any
+
+  private get message(): string {
+    return getErrorMessage(this.error)
+  }
+}
+</script>
