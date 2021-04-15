91afe4d804bce5855f298485b155f575dc9ada0a
Ildar Timerbaev <dkildar@gmail.com>
2021-04-15 23:54:29 +0300

    Implement base main page

--- src/components/Index/Home.vue
+++ src/components/Index/Home.vue
@@ -1,25 +1,36 @@
 <template>
-  <div class="hero height-full">
-    <Center>
-      <span class="logo iconfont icon-hivesigner"/>
-      <h4 class="m-0">hivesigner</h4>
-      <div class="mt-4 mb-7">
-        <h2 class="mb-6">You're in control of your keys.</h2>
-        <div class="mb-6">
+  <div class="container mx-auto px-4">
+    <div class="grid gap-40 grid-cols-2 items-center justify-center">
+      <div class="col-span-1">
+        <img :src="require('../../assets/img/home.png')" alt="">
+      </div>
+      <div class="col-span-1">
+        <div class="flex items-center mb-24">
+          <img class="block pr-5" :src="require('../../assets/img/logo.svg')" alt="">
+          <span class="font-bold text-5xl">Hivesigner</span>
+        </div>
+        <p class="text-xl text-gray pb-12">
+          Secure way to sign with Hivesigner. Best security for users and developers to integrate industry standard OAuth2.
+        </p>
+        <router-link to="/login" class="bg-primary hover:bg-primary-dark text-white p-4 inline-flex items-center rounded-md">
+          <img class="mr-2" :src="require('../../assets/img/icons/keychain.svg')">
+          Get started
+        </router-link>
+
+        <div class="flex items-center pt-32">
+          <locale-selector />
+
           <router-link
-            to="/login?redirect=accounts"
-            class="rounded-2 px-3 py-3 bg-primary text-white button-link"
+            v-for="item of menu"
+            :key="item.to"
+            class="px-5 text-gray text-lg hover:text-primary cursor-pointer"
+            :to="item.to"
           >
-            <span class="iconfont icon-shield"/>
-            Get started
+            {{ item.label }}
           </router-link>
         </div>
-        <!-- <p>
-          Hivesigner neither stores nor has access to your private keys.
-        </p> -->
       </div>
-      <Footer/>
-    </Center>
+    </div>
   </div>
 </template>
 
@@ -32,5 +43,14 @@ import { PACKAGE } from '../../consts'
 })
 export default class Home extends Vue {
   private pkg = PACKAGE
+
+  private get menu() {
+    return [
+      { label: 'Apps', to: '/apps' },
+      { label: 'Accounts', to: '/accounts' },
+      { label: 'Documentation', to: '/wiki' },
+      { label: 'About', to: '/about' },
+    ]
+  }
 }
 </script>
---
+++ src/components/LocaleSelector.vue
@@ -0,0 +1,42 @@
+<template>
+  <Dropdown ref="dropdown" with-chevron trigger-class="text-black-light hover:text-primary">
+    <template slot="trigger">
+      <span class="text-lg uppercase">{{ locale }}</span>
+    </template>
+    <a
+      role="button"
+      class="cursor-pointer hover:text-primary"
+      v-for="locale of locales"
+      @click="onLocaleSelect(locale)"
+    >
+      {{ locale }}
+    </a>
+  </Dropdown>
+</template>
+
+<script lang="ts">
+import { Component, Ref, Vue } from 'nuxt-property-decorator'
+import Dropdown from './UI/Dropdown.vue'
+
+@Component({
+  components: { Dropdown }
+})
+export default class LocaleSelector extends Vue {
+  @Ref('dropdown')
+  private dropdownRef!: Dropdown
+
+  private locale = 'eng'
+
+  private get locales(): string[] {
+    return [
+      'ENG',
+      'RUS',
+      'FR'
+    ]
+  }
+
+  private onLocaleSelect(locale: string): void {
+    this.dropdownRef.hide()
+  }
+}
+</script>
