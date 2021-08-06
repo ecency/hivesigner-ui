1e6ec62b0db9cf24ce8396e0909f2eb62a798b7e
feruz <fmiboy@gmail.com>
2021-08-07 00:22:59 +0300

    condition

--- src/plugins/bugsnag.ts
+++ src/plugins/bugsnag.ts
@@ -1,7 +1,7 @@
 import Bugsnag from '@bugsnag/js'
 import BugsnagPluginVue from '@bugsnag/plugin-vue'
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' && process.env.BUGSNAG_API_KEY) {
   Bugsnag.start({
     apiKey: process.env.BUGSNAG_API_KEY as string,
     plugins: [new BugsnagPluginVue()]
