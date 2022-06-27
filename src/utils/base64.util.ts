22d2953bf1babbe49d19fe6859faf38710da4bee
feruz <fmiboy@gmail.com>
2022-06-27 21:25:25 +0300

    fix formatting

--- src/utils/base64.util.ts
+++ src/utils/base64.util.ts
@@ -1,11 +1,10 @@
 import { Buffer } from 'buffer'
 import { b64uLookup } from '~/consts'
 
-
 export function b64uEnc (str: string): string {
-  return Buffer.from(str, 'utf8').toString('base64').replace(/(\+|\/|=)/g, m => b64uLookup[m]);
+  return Buffer.from(str, 'utf8').toString('base64').replace(/(\+|\/|=)/g, m => b64uLookup[m])
 }
 
 export function b64uDec (str: string): string {
-  return Buffer.from(str, 'base64').toString('utf8').replace(/(-|_|\.)/g, m => b64uLookup[m]);
+  return Buffer.from(str, 'base64').toString('utf8').replace(/(-|_|\.)/g, m => b64uLookup[m])
 }
