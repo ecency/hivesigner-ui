16d30f2b03ff5170301280e6c70e4a4a598faff5
feruz <fmiboy@gmail.com>
2022-06-27 21:19:05 +0300

    update btoa and atob

--- src/utils/base64.util.ts
+++ src/utils/base64.util.ts
@@ -1,9 +1,10 @@
 import { b64uLookup } from '~/consts'
+import { Buffer } from 'buffer'
 
 export function b64uEnc (str: string): string {
-  return btoa(str).replace(/(\+|\/|=)/g, m => b64uLookup[m])
+  return Buffer.from(str, 'utf8').toString('base64').replace(/(\+|\/|=)/g, m => b64uLookup[m]);
 }
 
 export function b64uDec (str: string): string {
-  return atob(str.replace(/(-|_|\.)/g, m => b64uLookup[m]))
+  return Buffer.from(str, 'base64').toString('utf8').replace(/(-|_|\.)/g, m => b64uLookup[m]);
 }
