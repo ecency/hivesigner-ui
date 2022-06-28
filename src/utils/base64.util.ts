ec7023c7d13652aa03c2a48485b53368e0571eb4
feruz <fmiboy@gmail.com>
2022-06-28 11:17:40 +0300

    try js-base

--- src/utils/base64.util.ts
+++ src/utils/base64.util.ts
@@ -1,10 +1,10 @@
-import { Buffer } from 'buffer'
+import { Base64 } from 'js-base64';
 import { b64uLookup } from '~/consts'
 
 export function b64uEnc (str: string): string {
-  return Buffer.from(str, 'utf8').toString('base64').replace(/(\+|\/|=)/g, m => b64uLookup[m])
+  return Base64.atob(str).replace(/(\+|\/|=)/g, m => b64uLookup[m])
 }
 
 export function b64uDec (str: string): string {
-  return Buffer.from(str, 'base64').toString('utf8').replace(/(-|_|\.)/g, m => b64uLookup[m])
+  return Base64.btoa(str).replace(/(-|_|\.)/g, m => b64uLookup[m])
 }
