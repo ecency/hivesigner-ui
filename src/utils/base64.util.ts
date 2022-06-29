ae7b33dbf0ecf7b7cff43ed54b4969a98eba2dbf
Ildar Timerbaev <dkildar@gmail.com>
2022-06-30 00:07:46 +0300

    Fixed Base64 encoding and decoding

--- src/utils/base64.util.ts
+++ src/utils/base64.util.ts
@@ -2,9 +2,9 @@ import { Base64 } from 'js-base64'
 import { b64uLookup } from '~/consts'
 
 export function b64uEnc (str: string): string {
-  return Base64.btoa(str).replace(/(\+|\/|=)/g, m => b64uLookup[m])
+  return Base64.encode(str).replace(/(\+|\/|=)/g, m => b64uLookup[m])
 }
 
 export function b64uDec (str: string): string {
-  return Base64.atob(str).replace(/(-|_|\.)/g, m => b64uLookup[m])
+  return Base64.decode(str).replace(/(-|_|\.)/g, m => b64uLookup[m])
 }
