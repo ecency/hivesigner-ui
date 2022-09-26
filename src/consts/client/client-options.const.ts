5f22c9ad3f09f6bfc0c34c3af23ec62c9ca3b371
feruz <fmiboy@gmail.com>
2022-09-26 20:27:53 +0300

    testnet chainId change

--- src/consts/client/client-options.const.ts
+++ src/consts/client/client-options.const.ts
@@ -5,5 +5,5 @@ export const CLIENT_OPTIONS = {
   failoverThreshold: 15,
   consoleOnFailover: true,
   addressPrefix: network === 'testnet' ? 'TST' : 'STM',
-  chainId: network === 'testnet' ? '18dcf0a285365fc58b71f18b3d3fec954aa0c141c44e4e5cb4cf777b9eab274e' : 'beeab0de00000000000000000000000000000000000000000000000000000000'
+  chainId: network === 'testnet' ? '4200000000000000000000000000000000000000000000000000000000000000' : 'beeab0de00000000000000000000000000000000000000000000000000000000'
 }
