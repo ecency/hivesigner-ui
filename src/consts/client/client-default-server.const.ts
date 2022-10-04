c9864baf5179db33ae6e28ab2b8cb879a0717b4d
feruzm <fmiboy@gmail.com>
2022-10-05 00:42:43 +0500

    Revert "change server order"
    This reverts commit 8f023b9146e99f97926c8710796343189d94c0d0.

--- src/consts/client/client-default-server.const.ts
+++ src/consts/client/client-default-server.const.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_SERVER = [
-  'https://api.deathwing.me',
   'https://rpc.ecency.com',
-  'https://rpc.ausbit.dev'
+  'https://rpc.ausbit.dev',
+  'https://api.deathwing.me'
 ]
 export const DEFAULT_TESTNET_SERVER = [
   'https://testnet.openhive.network'
