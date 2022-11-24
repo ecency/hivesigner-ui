79eb0af1724f8432160db2140c655ced4a6eb448
feruzm <fmiboy@gmail.com>
2022-11-24 08:31:20 +0530

    add anyx.io

--- src/consts/client/client-default-server.const.ts
+++ src/consts/client/client-default-server.const.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_SERVER = [
   'https://rpc.ecency.com',
   'https://anyx.io',
-  'https://rpc.ausbit.dev',
+  'https://rpc.ausbit.dev'
 ]
 export const DEFAULT_TESTNET_SERVER = [
   'https://testnet.openhive.network'
