88a31ffcabe7e11ef1ac1091fed8aa01216dbe90
feruz <fmiboy@gmail.com>
2023-09-18 22:45:04 +0300

    improve logic

--- src/utils/check-app-integrity.util.ts
+++ src/utils/check-app-integrity.util.ts
@@ -2,7 +2,6 @@ export function isValidAppIntegrity (): boolean {
   return [
     'https://hivesigner.com',
     'https://staging.hivesigner.com',
-    'https://testnet.hivesigner.com',
-    ...(process.env.NODE_ENV !== 'production' ? ['http://localhost:3000'] : [])
-  ].includes(window.origin)
+    'https://testnet.hivesigner.com'
+  ].includes(window.origin) || process.env.NODE_ENV !== 'production'
 }
