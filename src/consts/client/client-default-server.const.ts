0180abd551614f2956204b7bc9a0015d94003ebf
Feruz M <fmiboy@gmail.com>
2025-05-25 15:58:36 +0300

    Update client-default-server.const.ts

--- src/consts/client/client-default-server.const.ts
+++ src/consts/client/client-default-server.const.ts
@@ -1,8 +1,6 @@
 export const DEFAULT_SERVER = [
   'https://api.hive.blog',
-  'https://rpc.ecency.com',
-  'https://anyx.io',
-  'https://rpc.ausbit.dev'
+  'https://api.deathwing.me'
 ]
 export const DEFAULT_TESTNET_SERVER = [
   'https://testnet.openhive.network'
